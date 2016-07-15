899907cedab3cbec397327c2346fa95013a84506
Ricardo Oyarzun <royarzun@gmail.com>
2016-07-15 00:16:12 -0400

    Update MainActivity tellJoke() method
    Make the toast message stay for longer on screen.
    Update joke getter call. This commit closes #2

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,19 +1,24 @@
 package com.udacity.gradle.builditbigger;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.udacity.gradle.jokes.JokeGenerator;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends AppCompatActivity {
+
+    private JokeGenerator jokeGenerator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        jokeGenerator = new JokeGenerator();
     }
 
 
@@ -40,7 +45,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, jokeGenerator.getJoke(), Toast.LENGTH_LONG).show();
     }
 
 
