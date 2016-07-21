751110dbdad3122bf092247dd4f5ebe9e0885162
Ricardo Oyarzun <royarzun@gmail.com>
2016-07-21 18:39:40 -0400

    Add functional test for the app
    closes #5

---
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -0,0 +1,29 @@
+
+package com.udacity.gradle.builditbigger;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.ExecutionException;
+
+
+@RunWith(AndroidJUnit4.class)
+public class AsyncTaskTest  {
+
+    @Test
+    public void testAsyncTaskWithNull(){
+        String testString = null;
+        try {
+            testString = new JokeAsyncTask().execute().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        Assert.assertNotNull(testString);
+    }
+
+}
